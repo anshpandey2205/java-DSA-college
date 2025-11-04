f8f8f1bc3f4bea733d8d4b9ce1b62f9677c4dc20
anshpandey2205 <anshupandey2227@gmail.com>
2025-11-04 09:54:42 +0530

    .

--- Lect 1/hello.java
+++ Lect 1/hello.java
@@ -1,7 +1,9 @@
 public class hello{
     public static void main(String[] args) {
-        byte a=15;
+        byte a=127;
+        boolean c = true;
         System.out.println(a);
+        System.out.println(c);
         System.out.println("hello");
     }
 }
---
+++ Lect 1/if_else.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class if_else {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the number");
+        int n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j>0;j--){
+        //         System.out.print("5");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j>0;j--){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=n;k>i;k--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+    }
+}
