d942500715604422af4d3bc04e7657f5d3078c11
anshpandey2205 <anshupandey2227@gmail.com>
2025-12-08 21:39:28 +0530

    .

---
+++ lect14/replaceOdd.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class replaceOdd {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
+        ListIterator<Integer> it =list.listIterator();
+        while(it.hasNext()){
+            Integer x=it.next();
+            if(x%2!=0){
+                it.set(-1);
+            }
+        }
+        System.out.println(list);
+    }
+}
