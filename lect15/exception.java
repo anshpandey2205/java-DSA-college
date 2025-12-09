e65d01ef155b7932ca86a3365a156215fd226f40
anshpandey2205 <anshupandey2227@gmail.com>
2025-12-09 14:12:09 +0530

    lect15

---
+++ lect15/exception.java
@@ -0,0 +1,13 @@
+package lect15;
+
+public class exception {
+    public static void main(String[] args) {
+        // exception error
+        System.out.println("hello");
+        int a=5;
+        int b=0;
+        int c=a/b;
+        System.out.println(c);
+        System.out.println("byeee");
+    }
+}
--- lect15/exceptionHandling.java
+++ lect15/exceptionHandling.java
@@ -2,12 +2,17 @@ package lect15;
 
 public class exceptionHandling {
     public static void main(String[] args) {
-        // exception error
         System.out.println("hello");
         int a=5;
         int b=0;
-        int c=a/b;
-        System.out.println(c);
+
+        // used for exception handling by this next code will run even if there is an exception
+        try {
+            int c=a/b;
+            System.out.println(c);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
         System.out.println("byeee");
     }
 }
