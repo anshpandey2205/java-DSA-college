c45288203eb664f79f75faf9d79625da579d06ee
anshpandey2205 <anshupandey2227@gmail.com>
2025-12-10 10:48:59 +0530

    lect16

--- lect16/pathKnown.java
+++ lect16/pathKnown.java
@@ -1,11 +1,14 @@
 package lect16;
-import java.io.*;
-public class hello {
+
+import java.io.File;
+
+public class pathKnown {
     public static void main(String[] args) {
-        File file=new File("New.txt");
+        File file=new File("data.java");
         try{
         if(file.createNewFile()){
-            System.out.println("file created");
+            System.out.println("file created"+file.getName());
+            System.out.println("file created"+file.getAbsoluteFile());
         }else{
             System.out.println("file already exist");
         }
