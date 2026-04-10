59d24133e840e4af46e23319670c25419ca79fe7
anshpandey2205 <anshupandey2227@gmail.com>
2026-04-10 10:43:22 +0530

    lect22

--- lect22/maxnum.java
+++ lect22/maxnum.java
@@ -1,18 +1,16 @@
 public class maxnum {
-    // a2c3d9e5 inme 9 is max sor print 9
+    // a2c3d9e5 inme 9 is max sor print 9 easy method
     public static void main(String[] args) {
         String s = "a2c3d9e5";
         char[] c = s.toCharArray();
-        int max = 0;
-        for (int i = 0; i < c.length; i++) {
-            if (Character.isDigit(c[i])) {
-                int num = c[i] - '0';
-                if (num > max) {
-                    max = num;
-                }
+        char max = c[1];
+        for (int i = 1; i < c.length; i += 2) {
+            if (c[i] > max) {
+                max = c[i];
             }
         }
         System.out.println(max);
+        
         
     }
 }
