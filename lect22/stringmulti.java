eee4da398082c094a0ebb54500e21ab80baeda7c
anshpandey2205 <anshupandey2227@gmail.com>
2026-04-10 11:32:14 +0530

    lect22

---
+++ lect22/stringmulti.java
@@ -0,0 +1,12 @@
+public class stringmulti {
+    // multiply two string no. like "123" and "456" = 56088 easy code
+    public static void main(String[] args) {
+        String s1 = "12";
+        String s2 = "3";
+        int num1 = Integer.parseInt(s1);
+        int num2 = Integer.parseInt(s2);
+        int result = num1 * num2;
+        System.out.println(result);
+        
+    }
+}
