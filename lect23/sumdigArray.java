3d8ae992f3d4f2f6b35c3a1667170bfd9934863d
anshpandey2205 <anshupandey2227@gmail.com>
2026-04-13 11:35:56 +0530

    lect23

---
+++ lect23/sumdigArray.java
@@ -0,0 +1,16 @@
+package lect23;
+
+public class sumdigArray {
+    public static int sumDigArray(int []arr,int i){
+        if(i==arr.length){
+            return 0;
+        }
+        return arr[i]+sumDigArray(arr, i+1);
+    }
+
+    public static void main(String[] args) {
+        int []arr={1,2,3,4,5,6,7,8,9};
+        int i=0;
+        System.out.println(sumDigArray(arr,i));
+    }
+}
