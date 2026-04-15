33e1e9476a74b09dd73ebda96f1b7dfb9519c461
anshpandey2205 <anshupandey2227@gmail.com>
2026-04-15 11:21:19 +0530

    lect24

---
+++ lect24/subStr.java
@@ -0,0 +1,20 @@
+package lect24;
+
+public class subStr {
+    static void subStr(String str, int i, String ans) {
+        if(i == str.length()) {
+            System.out.println(ans);
+            return;
+        }
+        char c = str.charAt(i);
+        // include
+        subStr(str, i + 1, ans + c);
+        // exclude
+        subStr(str, i + 1, ans);
+    }
+    public static void main(String[] args) {
+        String str = "abc";
+        subStr(str, 0, "");
+        
+    }
+}
