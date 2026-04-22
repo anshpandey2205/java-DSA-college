04bb8b54fc9d833eade1c10f7907463d1c5ee345
anshpandey2205 <anshupandey2227@gmail.com>
2026-04-22 10:31:31 +0530

    lect25

---
+++ lect25/sum.java
@@ -0,0 +1,20 @@
+package lect25;
+
+public class sum {
+    public static void subArraySum(int[]arr,int i,int sum){
+        if(i==arr.length){
+            System.out.println(sum);
+            return;
+        }
+        // include
+        subArraySum(arr, i+1, sum+arr[i]);
+        // exclude
+        subArraySum(arr, i+1, sum);
+        
+    }
+    public static void main(String[] args) {
+        int[]arr={1,2,3};
+        subArraySum(arr, 0, 0);
+        
+    }
+}
